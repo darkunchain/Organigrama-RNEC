1c5700c9254325eeb7b76d0b6dfa19360bed2c93
lfprieto <lfprieto@registraduria.gov.co>
2025-01-23 15:21:17 -0500

    modulos y componentes

---
+++ src/app/modules/organigrama/organigrama.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OrganigramaRoutingModule } from './organigrama-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    OrganigramaRoutingModule
+  ]
+})
+export class OrganigramaModule { }
